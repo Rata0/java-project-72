066016214545f0362ec56133d03ea96d88de65a7
Alexander <nekon689@gmail.com>
2024-11-10 14:20:07 +0300

    create initial database structure with the required table

---
+++ app/src/main/java/hexlet/code/DatabaseConfig.java
@@ -0,0 +1,48 @@
+package hexlet.code;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseConfig {
+    private static HikariDataSource hikariDataSource;
+
+    public static DataSource getDataSource() {
+        if (hikariDataSource == null) {
+            HikariConfig config = new HikariConfig();
+
+            String jdbcUrl = getJdbcUrl();
+            config.setJdbcUrl(jdbcUrl);
+            config.setUsername("postgres");
+            config.setPassword("12345");
+
+            hikariDataSource = new HikariDataSource(config);
+            createSchema();
+        }
+        return hikariDataSource;
+    }
+
+    private static String getJdbcUrl() {
+        return System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:Url;DB_CLOSE_DELAY=-1");
+    }
+
+    private static void createSchema() {
+        String sql = "CREATE TABLE IF NOT EXISTS urls ("
+                + "id SERIAL PRIMARY KEY,"
+                + "name VARCHAR(255) NOT NULL,"
+                + "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
+                + ")";
+
+        try (Connection connection = hikariDataSource.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+            System.out.println("Таблица urls успешно создана.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
