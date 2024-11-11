2d7f110ae79f9142ec0802013ac1ca79d6572511
Alexander <nekon689@gmail.com>
2024-11-11 22:10:02 +0300

    Templating engine instance created, implemented a template for the main page output

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,19 +2,17 @@ package hexlet.code;
 
 import hexlet.code.model.Url;
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+
+import gg.jte.ContentType;
+import gg.jte.TemplateEngine;
+import gg.jte.resolve.ResourceCodeResolver;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.sql.DataSource;
 
 @Slf4j
 public class App {
-    public static Javalin getApp() {
-        Javalin app = Javalin.create(config -> config.bundledPlugins.enableDevLogging())
-                .get("/", ctx -> ctx.result("Hello World"));
-
-        log.info("Обработка по пути /");
-        return app;
-    }
 
     public static void main(String[] args) {
         DataSource dataSource = DatabaseConfig.getDataSource();
@@ -25,5 +23,25 @@ public class App {
         urlRepository.addUrl(newUrl);
 
         getApp().start();
+    }
+
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte(createTemplateEngine()));
+        });
+
+        app.get("/", ctx -> {
+            ctx.render("index.jte");
+            log.info("Обрабокта пути /");
+        });
+
+        return app;
+    }
+
+    private static TemplateEngine createTemplateEngine() {
+        ClassLoader classLoader = App.class.getClassLoader();
+        ResourceCodeResolver codeResolver = new ResourceCodeResolver("templates", classLoader);
+        return TemplateEngine.create(codeResolver, ContentType.Html);
     }
 }
