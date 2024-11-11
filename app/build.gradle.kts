e476d47ff691beb9747d294d3fa461f2c887acda
Alexander <nekon689@gmail.com>
2024-11-11 22:04:29 +0300

    Added javalin and jte dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,8 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
 
     implementation("io.javalin:javalin:6.3.0")
+    implementation("io.javalin:javalin-bundle:6.3.0")
+    implementation("io.javalin:javalin-rendering:6.3.0")
 
     implementation("org.slf4j:slf4j-api:2.0.7")
     implementation("org.slf4j:slf4j-simple:2.0.7")
@@ -27,6 +29,8 @@ dependencies {
     implementation("com.h2database:h2:2.2.224")
     implementation("com.zaxxer:HikariCP:5.1.0")
     implementation("org.postgresql:postgresql:42.7.2")
+
+    implementation("gg.jte:jte:3.1.9")
 }
 
 tasks.test {
