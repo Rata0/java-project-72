6bae7bc7b287c6f48b736f834786da80916ecfc8
Alexander <nekon689@gmail.com>
2024-11-13 21:00:26 +0300

    Method that normalizes the reference

--- app/src/main/java/hexlet/code/controller/UrlController.java
+++ app/src/main/java/hexlet/code/controller/UrlController.java
@@ -1,12 +1,30 @@
 package hexlet.code.controller;
 
+import hexlet.code.model.Url;
+import hexlet.code.repository.UrlRepository;
 import io.javalin.http.Context;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.SQLException;
 
 public class UrlController {
-    public static void create(Context ctx) throws SQLException {
+    public static void create(Context ctx) throws URISyntaxException, SQLException {
         String url = ctx.formParam("url");
-        System.out.println(url);
+        URI uri = new URI(url);
+        StringBuilder domain = new StringBuilder(uri.getScheme())
+                .append("://")
+                .append(uri.getHost());
+
+        if (uri.getPort() != -1) {
+            domain.append(":").append(uri.getPort());
+        }
+
+        boolean check = UrlRepository.find(domain.toString());
+        if (!check) {
+            Url url1 = new Url(domain.toString());
+            System.out.println(url1.toString());
+            UrlRepository.save(url1);
+        }
     }
 }
