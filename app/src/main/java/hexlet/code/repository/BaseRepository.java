6bf1187b311dada56f2869bce1466eb49b99a804
Alexander <nekon689@gmail.com>
2024-11-13 20:14:27 +0300

    Created dto class BasePage

--- app/src/main/java/hexlet/code/repository/BaseRepository.java
+++ app/src/main/java/hexlet/code/repository/BaseRepository.java
@@ -1,17 +1,7 @@
 package hexlet.code.repository;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
+import com.zaxxer.hikari.HikariDataSource;
 
-public abstract class BaseRepository {
-    protected static DataSource dataSource;
-
-    public BaseRepository(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-
-    protected Connection getConnection() throws SQLException {
-        return dataSource.getConnection();
-    }
+public class BaseRepository {
+    public static HikariDataSource dataSource;
 }
